4213bc2d3b39252c3e992e0b4bd78cc23c12ec60
Bartolomeu Spegiorin Gusella <ccbarto@gmail.com>
2020-10-19 23:31:06 -0300

    validate data

--- controller/Exchange.controller.js
+++ controller/Exchange.controller.js
@@ -1,13 +1,43 @@
 import { getCurrencyConversionRate } from "../services";
 
 exports.makeConversion = async function (req, res) {
-  const {
+  const { clienID, currencyOrigin, amount, currencyTarget } = req.body;
+
+  // This value is on the fee charged to carry out the transaction
+  // e.g To set the fee to 5% set this value to 0.05  =  5% => 5 / 100 => 0.05
+  // for development the fee is setted to 0
+  const exchangeFee = 0;
+
+  const ratesRequest = await getCurrencyConversionRate(currencyOrigin);
+
+  if (!ratesRequest.ok) {
+    // confere body used e retorna erro do servidor
+    return res.send(500, "Operation Unavailable");
+  }
+
+  const ratesResponse = await ratesRequest.json();
+
+  if (!ratesResponse || !ratesResponse.rates)
+    return res.send(500, "Operation Unavailable");
+
+  if (!(currencyTarget.toUpper() in ratesResponse.rates))
+    return res.send(400, "currencyTarget not valid");
+
+  const rateTransaction = ratesResponse[currencyTarget.toUpper()];
+  const amountTarget = (
+    parseFloat(amount) *
+    rateTransaction *
+    (1 - exchangeFee)
+  ).toFixed(2);
+
+  const transactionData = {
     clienID,
     currencyOrigin,
-    amount,
+    amountOrigin: amount,
     currencyTarget,
-    exchangeTax,
-  } = req.body;
-
-  const rates = await getCurrencyConversionRate(currencyOrigin);
+    amountTarget,
+    rateTransaction,
+    exchangeFee,
+    timestamp: new Date(),
+  };
 };
